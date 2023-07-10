afb84ba7c89d6b02e9c81236efd459fc5d66aef5
TheerametKhonmun <139094364+TheerametKhonmun@users.noreply.github.com>
2023-07-10 10:38:44 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ int main()
+{ 
+cout<<"   Student Code : 6606021620155"<<endl;
+cout<<"   Student Name : Theeramet Khonmun "<<endl;
+cout<<"       Division : Information Technology"<<endl;
+cout<<"           Room : c"<<endl;
+cout<<"            sex : male"<<endl;
+cout<<"            Age : 18"<<endl;
+     system("pause");
+	return(0);
+}
