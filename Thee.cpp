9528515d685047ef95b807b31d9f4b43d38f5243
TheerametKhonmun <139094364+TheerametKhonmun@users.noreply.github.com>
2023-07-17 10:08:10 +0700

    Add files via upload

---
+++ Thee.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() 
+{
+	string Name;
+	int birth ;
+	cout<<"Enter name :";
+	cin>>Name;
+	cout<<"Enter yaer of birth :";
+	cin>>birth;
+	cout<<"Your age ="<<2566-birth<<"year old \n";
+	system("pause"); 
+	return (0);
+}
