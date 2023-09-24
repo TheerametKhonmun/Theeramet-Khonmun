006c1e601f8f0acb40bde5d62100e53736b5a83c
TheerametKhonmun <139094364+TheerametKhonmun@users.noreply.github.com>
2023-09-24 19:49:40 +0700

    Add files via upload

--- 6606021620155.cpp
+++ 6606021620155.cpp
@@ -1,36 +1,19 @@
 #include <iostream>
-#include <iomanip>
-#include <string>
+#include <time.h>
 using namespace std;
 int main()
 {
-int num[4],total=0,max=0,min=0;
-string name[4];
-for(int i=0;i<=3;i++)
-{	cout<<"Name["<<i<<"]:";
-	cin>>name [i];
-	cout<<"Score["<<i<<"]:";
-	cin>>num[i];
+	cout << "Element Value Histogram\n\n";
+	srand(time(NULL));
+	for (int i = 0; i < 10 ; i++)
+{
+	int r = 1+rand() % 30;
+	cout << "	" << i << "	"<< r <<"	";
+	for ( int j = 0 ; j < r ; ++j)
+		{	cout << "*";
+		}
+		cout <<endl;
 }
-	cout<<"-------------------------"<<endl;
-	cout<<"Name		Score"<<endl;
-	cout<<"-------------------------"<<endl;
-for(int i=0;i < 4;i++){
-	cout<<name[i]<<"		"<<num[i]<<endl;
-	total = total + num[i];
-	if(num[i] > max)
-		max = num[i];
-	min=num[0];
-}
-{for(int i=0; i<4;i++)
-	if(num[i] < min)
-		min = num[i];
-}
-	cout<<"-------------------------"<<endl;
-	cout<<"Max Score= "<<max<<endl;
-	cout<<"Min Score= "<<min<<endl;
-	cout<<"Total Score = "<<total<<endl;
-	cout<<"Average Score = "<<(float)total/4<<endl;
 	
 	system("pause");
 	return 0;
