85953f6eb153035dced19bd256ff52985773d653
TheerametKhonmun <139094364+TheerametKhonmun@users.noreply.github.com>
2023-09-25 10:24:28 +0700

    Add files via upload

--- 6606021620155.cpp
+++ 6606021620155.cpp
@@ -1,20 +1,78 @@
 #include <iostream>
-#include <time.h>
+#include <iomanip>
 using namespace std;
+int num , quiz[10] , mid[10] , final[10] ;
+void input();
+void view();
+void viewID();
 int main()
 {
-	cout << "Element Value Histogram\n";
-	srand(time(NULL));
-	for (int i = 0; i < 10 ; i++)
-{
-	int r = 1+rand() % 30;
-	cout << "	" << i << "	"<< r <<"	";
-	for ( int j = 0 ; j < r ; ++j)
-		{	cout << "*";
-		}
-		cout <<endl;
+int  menu;
+do{
+cout << "===========================\n";
+cout << " Menu \n";
+cout << "===========================\n";
+cout << "1.Input student records\n";
+cout << "2.View all student records\n";
+cout << "3.Show a student by ID\n";
+cout << "4.Exit\n";
+cout << "Select menu : ";
+cin >> menu;
+if (menu == 1)
+{input();}
+else if (menu == 2)
+{view();}
+else if (menu == 3)
+{viewID();}
+}while(menu != 4);
+
+
+
+system("pause");
+return 0;
 }
-	
-	system("pause");
-	return 0;
+
+
+void input()
+{
+cout << "Input Number of Student : ";
+cin >> num;
+for (int i = 1 ; i <= num ; i++){
+cout << "Number "<< i <<endl;
+cout << " Input Quiz :";
+cin >> quiz[i];
+cout << " Input Midterm :";
+cin >> mid[i];
+cout << " Input Final :";
+cin >> final[i];
+}
+}
+
+void view()
+{
+cout << "--------------------------------\n";
+cout << "StdID Quiz Midterm Final\n";
+cout << "--------------------------------\n";
+for (int i = 1; i<= num ; i++)
+{cout << i << " " << quiz[i] << " " << mid[i] << " " << final[i] <<endl;}
+cout << "--------------------------------\n";
+}
+
+void viewID()
+{
+int id ;
+cout << "Input ID:";
+cin >> id;
+for ( int i = 1 ; i <=num ; i++)
+{if( id == i){
+cout << "--------------------------------\n";
+cout << "StdID Quiz Midterm Final\n";
+cout << "--------------------------------\n";
+cout << i << " " << quiz[i] << " " << mid[i] << " " << final[i] <<endl;
+cout << "--------------------------------\n";
+
+}
+else
+cout << "Your ID is not found\n";
+}
 }
